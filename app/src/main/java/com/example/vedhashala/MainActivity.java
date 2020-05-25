4a2c676b28825101c9a94574c9fc2338c3cda4df
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2020-05-25 15:22:00 +0530

    created caller activity

--- app/src/main/java/com/example/vedhashala/MainActivity.java
+++ app/src/main/java/com/example/vedhashala/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.vedhashala;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        button = findViewById(R.id.connectBtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(MainActivity.this, CallActivity.class);
+                startActivity(myIntent);
+            }
+
+        });
+
     }
 }
